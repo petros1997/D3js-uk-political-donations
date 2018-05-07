92aac9d2430b5218db0b57a439bd0680cf6dfc9e
Petros Skordas <p15skor@ionio.gr>
2018-05-07 20:33:54 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -405,7 +405,7 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
-	responsiveVoice.speak("The" + d.donor + "donated an amount of" + d.amount + "british pounds!");
+	responsiveVoice.speak("The" + donor + "donated an amount of" + amount + "british pounds!");
 	
 	
 	var img = document.createElement("img");
